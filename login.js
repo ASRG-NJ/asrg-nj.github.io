c48be99cc2470730c6c2a0d58f543fe8e9ffc539
ASRG-NJ <airstructure@pm.me>
2026-05-07 18:53:35 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,2 +1,13 @@
-<!doctype html>
-<html lang="en">
+document.addEventListener("DOMContentLoaded",()=>{
+
+  const form=document.getElementById("loginForm");
+  const error=document.getElementById("error");
+
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    error.textContent=
+      "Username and/or password does not exist.";
+  });
+
+});
